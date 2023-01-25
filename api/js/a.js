b3621cc75a06ce21748f0aadd29812a1aec20496
ubrong <ubrong@qq.com>
2023-01-25 22:13:20 +0800

    修正js

---
+++ api/js/a.js
@@ -0,0 +1,9 @@
+//export default function handler(req, res) {
+//    const { name = 'servless' } = req.query;
+//    return res.send(`Hello ${name}!`);
+//}
+
+export default async (req, res) => {
+	//const { name = 'servless' } = req.query;
+    return res.send(`this is api/js/a.js!`);
+}
